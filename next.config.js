84917e504838952c97ae329daebc2bf70e5e19f9
niraulabijay <bijaykn7@gmail.com>
2022-01-30 18:47:00 +0545

    prod build setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
   },
